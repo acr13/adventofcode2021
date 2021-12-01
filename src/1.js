1302b70beb7c4fa0e656581679aafef0ddf544d7
Alex Richardson <alex@alexcr.com>
2021-12-01 12:01:42 -0500

    improvementz

--- src/1.js
+++ src/1.js
@@ -1,34 +1,15 @@
 const fs = require('fs');
-
 const numbers = fs.readFileSync('./src/inputs/1.txt', 'UTF-8')
-    .split(/\r?\n/)
-    .map(Number);
+  .split(/\r?\n/)
+  .map(Number);
 
-const p1 = (numbers) => {
-  let count = 0;
+let p1 = 0;
+let p2 = 0;
 
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i] > numbers[i - 1]) {
-      count++;
-    }
-  }
+for (let i = 1; i < numbers.length; i++) {
+  if (numbers[i] > numbers[i - 1]) { p1++; }
+  if (i > 2 && numbers[i] > numbers[i - 3]) { p2++; }
+}
 
-  return count;
-};
-
-const p2 = (numbers) => {
-  let count = 0;
-
-  for (let i = 3; i < numbers.length; i++) {
-    const current = numbers[i] + numbers[i - 1] + numbers[i - 2];
-    const prev = numbers[i - 1] + numbers[i - 2] + numbers[i - 3];
-    if (current > prev) {
-      count++;
-    }
-  }
-
-  return count;
-};
-
-console.log('Part one:', p1(numbers));
-console.log('Part two:', p2(numbers));
+console.log('Part one:', p1);
+console.log('Part two:', p2);
