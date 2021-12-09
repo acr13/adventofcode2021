f507f5a4da082b9e83f9caf08adfff182ad55f42
acr13 <alex@alexcr.com>
2021-12-09 09:12:26 -0500

    Use proper row,col variables

--- src/9.js
+++ src/9.js
@@ -6,27 +6,26 @@ fs.readFileSync('./src/inputs/9.txt', 'UTF-8')
   .split(/\r?\n/)
   .map(line => grid.push(line.split('').map(Number)));
 
-const COLS = grid.length;
-const ROWS = grid[0].length;
+const ROWS = grid.length;
+const COLS = grid[0].length;
 
-const nearby = (grid, c, r) =>
+const nearby = (grid, r, c) =>
   [
-    [c - 1, r],
-    [c + 1, r],
-    [c, r - 1],
-    [c, r + 1],
-  ].filter(([c2, r2]) => c2 >= 0 && c2 < grid.length && r2 >= 0 && r2 < grid[0].length);
+    [r, c - 1],
+    [r, c + 1],
+    [r - 1, c],
+    [r + 1, c],
+  ].filter(([r2, c2]) => r2 >= 0 && r2 < grid.length && c2 >= 0 && c2 < grid[0].length);
 
-const isLowPoint = (grid, c, r) => nearby(grid, c, r).every(([c2, r2]) => grid[c2][r2] > grid[c][r]);
+const isLowPoint = (grid, r, c) => nearby(grid, r, c).every(([r2, c2]) => grid[r2][c2] > grid[r][c]);
 
-// step thru the grid, checking each cell if all nearby cells are less than the current cell
 const getLowPoints = (grid) => {
   const lows = [];
 
-  for (let c = 0; c < COLS; c++) {
-    for (let r = 0 ; r < ROWS; r++) {
-      if (isLowPoint(grid, c, r)) {
-        lows.push([c, r, grid[c][r]]);
+  for (let r = 0; r < ROWS; r++) {
+    for (let c = 0 ; c < COLS; c++) {
+      if (isLowPoint(grid, r, c)) {
+        lows.push([r, c, grid[r][c]]);
       }
     }
   }
@@ -38,21 +37,21 @@ const getBasins = (grid, lowPoints) => {
   const basins = [];
 
   for (let i = 0; i < lowPoints.length; i++) {
-    const [c, r, l] = lowPoints[i];
+    const [r, c, l] = lowPoints[i];
     
     // BFS
-    const nearbyCells = nearby(grid, c, r);
-    const visited = new Set(`${c},${r}`);
+    const nearbyCells = nearby(grid, r, c);
+    const visited = new Set(`${r},${c}`);
     const basin = [l];
 
     while (nearbyCells.length > 0) {
-      const [c2, r2] = nearbyCells.pop();
+      const [r2, c2] = nearbyCells.pop();
       
       // if this is good, check its neighbs
-      if (!visited.has(`${c2},${r2}`) && grid[c2][r2] > grid[c][r] && grid[c2][r2] <= 8) { // no 9's
-        visited.add(`${c2},${r2}`);
-        basin.push(grid[c2][r2]);
-        nearbyCells.push(...nearby(grid, c2, r2));
+      if (!visited.has(`${r2},${c2}`) && grid[r2][c2] > grid[r][c] && grid[r2][c2] <= 8) { // no 9's
+        visited.add(`${r2},${c2}`);
+        basin.push(grid[r2][c2]);
+        nearbyCells.push(...nearby(grid, r2, c2));
       }
     }
     
